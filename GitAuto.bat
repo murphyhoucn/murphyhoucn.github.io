f7635409cbbd92d2ac0462ef1a8aea6496a3ccf8
MurphyWinITX <cosmicdustycn@outlook.com>
2025-05-30 15:38:39 +0800

    Auto Commit by MurphyWinITX on 2025-05-30 15:38:38

--- GitAuto.bat
+++ GitAuto.bat
@@ -11,10 +11,21 @@ if not defined GITHUB_USER (
     exit /b
 )
 
+:: Get the current date and time
+for /f "tokens=2 delims==" %%a in ('wmic os get localdatetime /value') do set datetime=%%a
+set YEAR=%datetime:~0,4%
+set MONTH=%datetime:~4,2%
+set DAY=%datetime:~6,2%
+set HOUR=%datetime:~8,2%
+set MINUTE=%datetime:~10,2%
+set SECOND=%datetime:~12,2%
+set CURRENT_TIME=%YEAR%-%MONTH%-%DAY% %HOUR%:%MINUTE%:%SECOND%
+
 :: Execute Git commands
 git status
 git add .
-git commit -m "Auto Commit by %GITHUB_USER%"
+git commit -m "Auto Commit by %GITHUB_USER% on %CURRENT_TIME%"
 git push
 
 endlocal
+pause
